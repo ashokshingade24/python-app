7dac28aa22311af3f275b22a09bbcbb682b03c07
ashokshingade24 <ashokshingade24@gmail.com>
2018-04-11 18:35:25 +0530

    Update simpleapp.py

--- simpleapp.py
+++ simpleapp.py
@@ -10,7 +10,7 @@ start = int(round(time.time()))
 @app.route("/")
 def hello_world():
 
-    return "Hello world, Edited .."
+    return "Hello world, Edited by ashok .."
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage="python simpleapp.py -p ")
